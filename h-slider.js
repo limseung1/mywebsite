79fd62c898a5b9990744a3d79f7094ce20fe93ad
limseung1 <54563657+limseung1@users.noreply.github.com>
2021-01-24 14:08:04 +0900

    Add files via upload

--- h-slider.js
+++ h-slider.js
@@ -6,6 +6,13 @@ h_slide_contents = document.querySelectorAll(".skill");
 h_slide_triggers.forEach((slide, idx)=>{
     slide.addEventListener("click", function(){
 
+        // Slide Button Click Effect
+        h_slide_triggers.forEach((slide)=>{
+            slide.style["box-shadow"] = "none"
+        });
+
+        slide.style["box-shadow"]="0px 0px 4px 0px white"
+
         // Hide all slide contents
         h_slide_contents.forEach((content) => {
             content.classList.add("d-none");
