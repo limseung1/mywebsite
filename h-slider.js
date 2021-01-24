e5d70166f71bbb9913f2c1fd554240fca699de27
limseung1 <54563657+limseung1@users.noreply.github.com>
2021-01-24 14:39:46 +0900

    Add files via upload

--- h-slider.js
+++ h-slider.js
@@ -11,17 +11,21 @@ h_slide_triggers.forEach((slide, idx)=>{
             slide.style["box-shadow"] = "none"
         });
 
-        slide.style["box-shadow"]="0px 0px 4px 0px white"
+        slide.style["box-shadow"]="0px 0px 5px 0px white"
+        slide.style["transition"]="0.4s"
 
         // Hide all slide contents
         h_slide_contents.forEach((content) => {
-            content.classList.add("d-none");
-            content.classList.add("d-sm-block");
+            content.classList.add("skill-hidden");
+            content.classList.remove("skill-visible");
+            // content.classList.add("d-none");
+            // content.classList.add("d-sm-block");
             
         })
 
         // Display Specific slide content
-        h_slide_contents[idx].classList.remove("d-none")
-        h_slide_contents[idx].classList.remove("d-sm-block")
+        // h_slide_contents[idx].classList.remove("d-none")
+        // h_slide_contents[idx].classList.remove("d-sm-block")
+        h_slide_contents[idx].classList.add("skill-visible")
     })
 })
